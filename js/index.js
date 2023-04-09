6eb807910e06a15b94ec27815270bbc135d3eab9
Nicolo Contin <nicocontin@gmail.com>
2023-04-09 19:01:53 +0200

    create launchBall function and first brick

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ document.querySelector("h1").style.display = "none";
 canvas.style.display = "block";
 canvas.style.background = "black";
 
-//paddle code
+// PADDLE VARIABLES
 const paddleWidth = 120;
 const paddleHeight = 15;
 const paddleSpeedValue = 5;
@@ -46,34 +46,37 @@ function drawPaddle() {
   }
 }
 
-//ball code
+// BALL VARIABLES
 
-let ballX = 150;
-let ballY = 170;
-let ballRadius = 15;
-let ballSpeed = 2;
+// ball size
+const ballRadius = 10;
+// ball X and Y axis position
+let ballX = paddleX + paddleWidth / 2;
+let ballY = canvas.height - (paddleHeight + ballRadius);
+
+// speed of the ball
+let ballSpeed = 1;
+
+//ball velocity
 let ballDirectionX = ballSpeed;
-// ballSpeed * (Math.random() * 2 - 1); // Random trajectory
 let ballDirectionY = ballSpeed;
 
-function moveBall() {
-  ballX += ballDirectionX;
-  ballY += ballDirectionY;
-}
+let ballLaunched = false;
 
-// Collisions
-function ballCollision() {
-  if (ballX > canvas.width - ballRadius) {
-    ballDirectionX *= -1;
-  }
-  if (ballX < 0 + ballRadius) {
-    ballDirectionX *= -1;
-  }
-}
+// BRICKS VARIABLES
 
-function resetBall() {
-  ballX = paddleX + paddleWidth / 2;
-  ballY = canvas.height - 2 * paddleHeight;
+const brickX = 0;
+const brickY = 0;
+const brickHeight = 10;
+const brickWidth = 75;
+
+function drawBricks() {
+  ctx.beginPath();
+  // xPos, yPos, width, height
+  ctx.rect(brickX, brickY, brickWidth, brickHeight);
+  ctx.fillStyle = "red";
+  ctx.fill();
+  ctx.closePath();
 }
 
 function drawBall() {
@@ -84,8 +87,70 @@ function drawBall() {
   ctx.fill();
   ctx.closePath();
 }
-resetBall();
-// Moving Paddle
+
+function launchBall() {
+  // ball already in motion
+  // Set the direction of the ball
+
+  let angle = Math.PI / 2;
+  ballDirectionX = 0;
+  ballDirectionY = ballSpeed * Math.sin(angle);
+  // Set ballLaunched to true
+  ballLaunched = true;
+}
+
+function moveBall() {
+  // move the stationary ball with the paddle
+
+  if (!ballLaunched) {
+    // Set the ball position to the paddle position
+    ballX = paddleX + paddleWidth / 2;
+    ballY = canvas.height - (paddleHeight + ballRadius);
+  } else {
+    // Move the ball
+    ballX += ballDirectionX;
+    ballY -= ballDirectionY;
+  }
+
+  //change the direction of the ball after the collision
+  if (ballY + ballRadius > paddleY && ballX + ballRadius > paddleX && ballX < paddleX + paddleWidth) {
+    let deltaX = ballX + ballRadius - (paddleX + paddleWidth / 2);
+    ballDirectionX = (deltaX / (paddleWidth / 2)) * ballSpeed;
+  }
+}
+
+// Collisions
+function ballCollision() {
+  // if the ball hit the right wall
+  if (ballX > canvas.width - ballRadius) {
+    ballDirectionX *= -1;
+  }
+  // if the ball hit the left wall
+  if (ballX < 0 + ballRadius) {
+    ballDirectionX *= -1;
+  }
+  // if the ball hit the top wall
+  if (ballY < 0) {
+    ballDirectionY *= -1;
+  }
+  // if the ball hit the paddle
+  if (ballY + ballRadius > paddleY && ballX + ballRadius > paddleX && ballX < paddleX + paddleWidth) {
+    ballDirectionY *= -1;
+  }
+
+  // if the ball fall
+  if (ballY > canvas.height) {
+    ballLaunched = false;
+  }
+
+  // if the ball hit the bricks
+  if (ballY < brickY + brickHeight + ballRadius && ballX < brickX + brickWidth + ballRadius) {
+    ballDirectionY *= -1;
+  }
+}
+
+// event listener
+
 document.addEventListener("keydown", (event) => {
   console.log(event);
   if (event.code === "ArrowLeft") {
@@ -94,7 +159,8 @@ document.addEventListener("keydown", (event) => {
   if (event.code === "ArrowRight") {
     isPaddleGoingRight = true;
   }
-  if (ballDirectionX === 0 && ballDirectionY === 0 && event.code === "Space") {
+  if (event.code === "Space") {
+    ballLaunched = true;
   }
 });
 
@@ -103,20 +169,38 @@ document.addEventListener("keyup", (event) => {
   isPaddleGoingRight = false;
 });
 
+// moving paddle with the mouse
+
+document.addEventListener("mousemove", (event) => {
+  let mouseX = event.offsetX;
+  paddleX = mouseX - paddleWidth / 2;
+
+  if (paddleX < 0) {
+    paddleX = 0;
+  } else if (paddleX + paddleWidth > canvas.width) {
+    paddleX = canvas.width - paddleWidth;
+  }
+});
+
+//launch ball with the click
+
+canvas.addEventListener("click", (event) => {
+  if (!ballLaunched) {
+    launchBall();
+  }
+});
+
 console.log(event);
 
+//loop
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawPaddle();
   drawBall();
-  resetBall();
+  drawBricks();
   moveBall();
   ballCollision();
 
   animationFrameId = requestAnimationFrame(animate);
 }
 animate();
-
-if (ballDirectionX || ballDirectionY) {
-  drawBall();
-}
